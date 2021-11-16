d732fcba12bcbbd3933d1c3451b8ea56a1a10d9d
tomas-20 <tacuna20@stuy.edu>
2021-11-16 09:51:29 -0500

    made it so that the main function writes the array to a file and reads it from the file

--- randomness.c
+++ randomness.c
@@ -19,5 +19,16 @@ int main() {
     printf("%d ", randomwans[i]);
   }
   printf("\n");
+  int file = open("cheese", O_CREAT | O_WRONLY, 0600);
+  write(file, &randomwans, sizeof(int[10]));
+  close(file);
+  int randomtwos[10];
+  file = open("cheese", O_RDONLY);
+  read(file, &randomtwos, sizeof(int[10]));
+  close(file);
+  for (int i = 0; i < 10; i ++) {
+    printf("%d ", randomtwos[i]);
+  }
+  printf("\n");
   return 0;
 }
